2851f94db5a7df10a0ef576a1802edb19d25cace
Ivan <iivanov@soft2run.com>
2023-01-15 21:41:26 +0200

    formatting fix

--- main.tf
+++ main.tf
@@ -1,15 +1,14 @@
 terraform {
   cloud {
     organization = "Pro2type"
-
     workspaces {
       name = "Pro2type-Telerik"
     }
   }
 }
-  #An example resource that does nothing.
-     resource "null_resource" "example" {
-       triggers = {
-         value = "A example resource that does nothing!"
-       }
-     }
+#An example resource that does nothing.
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}

56a4f485c56b6d4f363b26fe07c652a09312bade
Ivan <iivanov@soft2run.com>
2023-01-19 21:41:15 +0200

    do configuration for terraform added

--- main.tf
+++ main.tf
@@ -4,11 +4,28 @@ terraform {
     workspaces {
       name = "Pro2type-Telerik"
     }
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
   }
 }
+variable "do_token" {}
+provider "digitalocean" {
+  token = var.do_token
+}
 #An example resource that does nothing.
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
+resource "digitalocean_kubernetes_cluster" "pro2type" {
+  name   = "pro2type"
+  region = "fra1"
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.25.4-do.0"
+
+  node_pool {
+    name       = "pro2type-pool-1"
+    size       = "s-1vcpu-2gb"
+    node_count = 3
   }
 }

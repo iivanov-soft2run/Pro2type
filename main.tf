016b4398cae2835c4e85423706f74944f9239ce0
Ivan <iivanov@soft2run.com>
2023-01-21 18:41:43 +0200

    fix chart name

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "digitalocean_project_resources" "pro2type" {
 resource "helm_release" "crossplane" {
   name             = "crossplane"
   repository       = "https://charts.crossplane.io/stable"
-  chart            = "crossplane-stable/crossplane"
+  chart            = "crossplane"
   version          = "1.10.2"
   namespace        = "crossplane"
   create_namespace = true
